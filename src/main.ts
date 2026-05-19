512a3df2b6a8a47d96e0b4a629a6494315ca496b
Aman Bind <119284895+amanbind898@users.noreply.github.com>
2026-05-19 10:37:09 +0530

    Add Ticker for rat spawning and score tracking

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { Application, Text, Graphics } from "pixi.js";
+import { Application, Text, Graphics, Ticker } from "pixi.js";
 import './style.css'
 
 (async () => {
     const app = new Application();
+    const ticker = new Ticker();
 
     await app.init({
         background: "#579ad8",
@@ -14,13 +15,18 @@ import './style.css'
     const title = new Text({text: "rat hole game",style: {fontSize: 32}});
     title.position.set(550,20);
     app.stage.addChild(title);
+    let scoreVal =0;
+    const score = new Text({text: "score : 00",style: {fontSize: 32}});
+    score.position.set(50,20);
+    app.stage.addChild(score);
    
     let x = 500;
     let y = 150;
     let space = 150;
 
     const holes = [];
-  const ratState:Array<boolean>  = new Array(9).fill(false);
+//   const ratState:Array<boolean>  = new Array(9).fill(false);
+
     for (let i = 0; i < 9; i++) {
         let row = Math.floor(i / 3);
         let col = i % 3;
@@ -30,39 +36,58 @@ import './style.css'
         app.stage.addChild(hole);
       
     }
-    let currRat ;
+  
     function spawnRat(X:number,Y:number){
-       if(currRat){
-        currRat.destroy();
-    }
-const rat = new Graphics().circle(X,Y,30).fill("#ebc10a")
-     app.stage.addChild(rat);
-     currRat=rat;
+  
+    const rat = new Graphics().circle(X,Y,30).fill("#ebc10a");
+    app.stage.addChild(rat);
+
+
      rat.eventMode="static";
-     rat.on("pointerdown",()=>{
-      console.log("mouse clicked")
-      rat.destroy();
-       setTimeout(() => {
-       
+     rat.on("pointerdown", () => {
+    console.log("mouse clicked");
+    rat.destroy();
+scoreVal++;
+});
+
+
+     setTimeout(() => {
             rat.destroy();
-            currRat = null;
-        
-    }, 500);
-     }
-    );
+    }, 700);
+    
     }
      
 
-     setInterval(()=>{ 
+    //  setInterval(()=>{ 
+    //   let random = Math.floor(Math.random() * 9);
+    //    let row=Math.floor(random/ 3);
+    //     let col=random%3;
+    //  let ratx=x+col*space;
+    // let raty=y+row*space;
+    // spawnRat(ratx,raty);
+    // ratState[random] = true; }, 2000);
+
+
+let spawnTimer = 0;
+
+ticker.add((Ticker)=>{
+  spawnTimer += Ticker.deltaTime;
+  if(spawnTimer>= 120)
+    {spawnTimer=0;
+      console.log("spawn")
       let random = Math.floor(Math.random() * 9);
        let row=Math.floor(random/ 3);
         let col=random%3;
-     let ratX=x+col*space;
-    let ratY=y+row*space;
-    spawnRat(ratX,ratY);
-    ratState[random] = true;
+     let ratx=x+col*space;
+    let raty=y+row*space;
+    spawnRat(ratx,raty);
+    }
+    
+    score.text=`score :${scoreVal}`
+})
 
-}, 2000);
+ticker.start();
+
 
 
 })();
